26a061a58e8a3e35835ef8e78c48d2e2a43217d1
James Shisiah <jamesshisiah@gmail.com>
2022-05-11 00:58:29 +0300

    Adds insertionSort

---
+++ src/insertionSort.ts
@@ -0,0 +1,30 @@
+class InsertionSort {
+
+  private arr: number[]
+
+  constructor(arr:number[]) {
+    this.arr = arr
+  }
+
+
+  // arrange the array in ascending order
+  insSort(): number[] { // Big O: O(n^2) - quadratic
+    let n = this.arr.length
+
+    for (let i = 1; i < n; i++) {
+      let key = this.arr[i]
+      let space = i
+      for (let j = space-1; j>=0; j--) {
+        if (this.arr[j] > key) {
+          this.arr[j+1] = this.arr[j]
+          space = j
+          continue
+        }
+        break
+      }
+      this.arr[space] = key
+    }
+
+    return this.arr
+  }
+}
